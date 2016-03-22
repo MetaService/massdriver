939eac85db0b088a403a6c670ad5a2b6282bfecb
Glenn West <glennswest@yahoo.com.sg>
2016-03-22 14:47:28 +0800

    Fixup removing old code

--- lib/massdriver.rb
+++ lib/massdriver.rb
@@ -1,5 +1,4 @@
 require "massdriver/version"
-require 'ruby-graphviz'
 
 module Massdriver
 
@@ -58,24 +57,4 @@ def newMq2RInstance(name,abbr,mst,qname,url,data)
 end
 
 
-SDS1 = g.add_node("StockDiscoveryService1")
-SDS2 = g.add_node("StockDiscoveryService2")
-smq = g.add_node("Discovery Message Queue");
-g.add_edges(SDS1,smq);
-g.add_edges(SDS2,smq);
-
-SHRS1 = g.add_node("StockHistoryRetrieverService1");
-SHRS2 = g.add_node("StockHistoryRetrieverService2");
-SHRS3 = g.add_node("StockHistoryRetrieverService3");
-SHRS4 = g.add_node("StockHistoryRetrieverService4");
-g.add_edges(smq,SHRS1);
-g.add_edges(smq,SHRS2);
-g.add_edges(smq,SHRS3);
-g.add_edges(smq,SHRS4);
-
-g.output(:png => "graph.png")
-
-
-
-  # Your code goes here...
 end
